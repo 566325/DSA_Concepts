d189009dd6f5640a9ce0b3eafcea02002500f7e0
566325 <nethisireesha288@gmail.com>
2023-10-03 21:45:52 +0530

    [Sireesha]:Added Simple queue

---
+++ QueuesEx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures_Concepts
+{
+    public class QueuesEx
+    {
+        Node Front;
+        Node rear;
+        
+        public void enqueData(int data)
+        {
+            Node NodeObj = new Node(data);
+            if(Front == null)
+            {
+                rear = NodeObj;
+                Front = rear;
+            }
+            else
+            {
+                rear.Next = NodeObj;
+                rear = rear.Next;
+            }
+        }
+        public void Display()
+        {
+            if(Front == null)
+            {
+                Console.WriteLine("Queue is Empty");
+            }
+            else
+            {
+                Node temp = Front;
+                while(temp != null)
+                {
+                    Console.WriteLine(temp.data + " ");
+                    temp = temp.Next;
+                }
+            }
+        }
+        
+    }
+}
