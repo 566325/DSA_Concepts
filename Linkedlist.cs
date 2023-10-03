60aa9ec8dcf8a3045d31f2b3cf7ffd2442722304
566325 <nethisireesha288@gmail.com>
2023-10-03 21:39:09 +0530

    [Sireesha]:Added Simple Linked List

---
+++ Linkedlist.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures_Concepts
+{
+    public class Linkedlist
+    {
+        public Node head;
+
+        public void AddNode(int data)
+        {
+            Node node = new Node(data);
+            if(this.head == null)
+            {
+                head = node;
+
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.Next != null)
+                {
+                    temp = temp.Next;
+
+                }
+                temp.Next = node;
+
+            }
+            Console.WriteLine("{0} is inserted into the Linkedlist", data);
+        }
+        public void Display()
+        {
+            if(head == null)
+            {
+                Console.WriteLine("Linked list is Empty");
+            }
+            else
+            {
+                Node tempnode = head;
+                while(tempnode != null)
+                {
+                    Console.WriteLine(tempnode.data + " ");
+                    tempnode = tempnode.Next;
+                }
+            }
+        }
+        public Node InsertAtPerticularPosition(int position ,int data)
+        {
+            if (position < 1)
+            {
+                Console.WriteLine("Invalid Position");
+            }
+            if (position == 1)
+            {
+                var temp = new Node(data);
+                temp.Next = head;
+                head = temp;
+            }
+            else
+            {
+                while(position != 0)
+                {
+                    if(position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.Next = this.head.Next;
+                        head.Next = node;
+                        break;
+                    }
+                    head = head.Next;
+                }
+                if(position != 1)
+                {
+                    Console.WriteLine("Position out of the Range");
+                }
+            }
+            return head;
+        }
+        public Node RemoveFirstNode()
+        {
+            if(this.head == null)
+            {
+                return null;
+            }
+            this.head =this.head.Next;
+            return this.head;
+        }
+        public Node RemoveLastNode()
+        {
+            if(head == null)
+            {
+                return null;
+            }
+            if(head.Next == null)
+            {
+                return null;
+            }
+            Node temp = head;
+            while(temp.Next.Next != null)
+            {
+                temp= temp.Next;
+            }
+            temp.Next = null;
+            return head;
+        }
+        public Node Search(int values)
+        {
+            while(this.head != null)
+            {
+                if (this.head.data == values)
+                {
+                    return this.head;
+                }
+                this.head=this.head.Next;
+            }
+            Console.WriteLine("donot match the values");
+            return null;
+
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿namespace DataStructures_Concepts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //LinkedList<int> list = new LinkedList<int>();
+            //list.AddFirst(56);
+            //list.AddLast(30);
+            //list.AddLast(70);
+
+            //foreach (int data in list)
+            //{
+            //    Console.WriteLine(data);
+            //}
+
+
+            Console.WriteLine("*************************");
+
+            Linkedlist linkedList = new Linkedlist();
+            linkedList.AddNode(56);
+            linkedList.AddNode(30);
+            linkedList.AddNode(70);
+            linkedList.Display();
+            //Console.WriteLine("Add first perticular positon in the list");
+            //linkedList.InsertAtPerticularPosition(1,80);
+            //linkedList.Display();
+            //Console.WriteLine("Remove the First element in the list");
+            //linkedList.RemoveFirstNode();
+            //linkedList.Display();
+            //Console.WriteLine("remove last element in the list");
+            //linkedList.RemoveLastNode();
+            //linkedList.Display();
+            linkedList.Search(78);
+            
+
+
+            //Stacks Obj= new Stacks();
+            //Obj.pushdata(70);
+            //Obj.pushdata(30);
+            //Obj.pushdata(56);
+            //Console.WriteLine("Elements in the Stack :");
+
+            //Obj.display();
+
+            //Queues
+            //QueuesEx Obj = new QueuesEx();
+            //Obj.enqueData(56);
+            //Obj.enqueData(30);
+            //Obj.enqueData(70);
+            //Console.WriteLine("Elements in the Queue are :");
+
+            //Obj.Display();
+
+        }
+    }
+}
